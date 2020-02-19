77495c4285af21b77281853b212a3a63d9d29826
Nordic Energy Dev <support@nordicenergy.io>
2020-02-19 16:41:46 +0000

    Initial commit
    Signed-off-by: Nordic Energy Dev <support@nordicenergy.io>

---
+++ build/precache-manifest.288ae3db98936996a7d1ac6c9d0693c1.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5dbe4ab8551f2bca8290b2ebd471363a",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e319d4a51282f74f76da",
+    "url": "./static/css/0.07de6c03.chunk.css"
+  },
+  {
+    "revision": "f53ec6405c0990027439",
+    "url": "./static/css/3.d75e0ccb.chunk.css"
+  },
+  {
+    "revision": "e319d4a51282f74f76da",
+    "url": "./static/js/0.b13cb03a.chunk.js"
+  },
+  {
+    "revision": "f53ec6405c0990027439",
+    "url": "./static/js/3.7c602e84.chunk.js"
+  },
+  {
+    "revision": "1d9f8c230e75659d8f9ddc1c02c9703d",
+    "url": "./static/js/3.7c602e84.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ebaaf0115722d928ed4",
+    "url": "./static/js/4.8084cfe5.chunk.js"
+  },
+  {
+    "revision": "10670a4c7ca36ea3c3c9",
+    "url": "./static/js/5.eb56b653.chunk.js"
+  },
+  {
+    "revision": "f3ec1788a3b9b758464f",
+    "url": "./static/js/6.3b0834fc.chunk.js"
+  },
+  {
+    "revision": "dda2a5f9fdc9106c6f3b",
+    "url": "./static/js/7.983f2d44.chunk.js"
+  },
+  {
+    "revision": "f1e4b16e79116493ae76",
+    "url": "./static/js/8.27266f31.chunk.js"
+  },
+  {
+    "revision": "ac3d7246eb564eb1233e",
+    "url": "./static/js/9.43c5e8af.chunk.js"
+  },
+  {
+    "revision": "ed4d81aa426012518398",
+    "url": "./static/js/main.83245d4d.chunk.js"
+  },
+  {
+    "revision": "05cf5a378883a6128c9a",
+    "url": "./static/js/runtime-main.0ab263c5.js"
+  },
+  {
+    "revision": "c0dedd2f2ed0c4d07d7ca75af3f0a65f",
+    "url": "./static/media/arrow-down-blue.c0dedd2f.svg"
+  },
+  {
+    "revision": "c0dedd2f2ed0c4d07d7ca75af3f0a65f",
+    "url": "./static/media/arrow-down-grey.c0dedd2f.svg"
+  },
+  {
+    "revision": "8de665f4812b2f482dfcfe0b97b50708",
+    "url": "./static/media/circle.8de665f4.svg"
+  },
+  {
+    "revision": "5e821f585e61fe82673ca8a053535c8d",
+    "url": "./static/media/dropdown-blue.5e821f58.svg"
+  },
+  {
+    "revision": "7d32d2fa19d17d6ab9f0e0067bebaf96",
+    "url": "./static/media/dropdown.7d32d2fa.svg"
+  },
+  {
+    "revision": "895d3eda717739c35e5a6c6e02aef5a9",
+    "url": "./static/media/dropup-blue.895d3eda.svg"
+  },
+  {
+    "revision": "802c6eacbb13c83b27c39be505228231",
+    "url": "./static/media/ethereum-logo.802c6eac.svg"
+  },
+  {
+    "revision": "674400972753804891ec372652944539",
+    "url": "./static/media/magnifying-glass.67440097.svg"
+  },
+  {
+    "revision": "d8e0be7d6efb0b53c37eb75e44b35bda",
+    "url": "./static/media/plus-blue.d8e0be7d.svg"
+  },
+  {
+    "revision": "d8e0be7d6efb0b53c37eb75e44b35bda",
+    "url": "./static/media/plus-grey.d8e0be7d.svg"
+  }
+]);
